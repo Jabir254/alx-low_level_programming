a9d48981e5bed903eb5abb63f68a1e41c911577c
jabir254 <jabirkullow@gmail.com>
2022-11-01 22:21:33 +0300

    fixed error on return

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,17 +9,13 @@
 
 char *_strchr(char *s, char c)
 {
-	int a;
-	while (1)
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
 	{
-		a = *s++;
-		if (a == c)
-		{
-			return (s - 1);
-		}
-		if (a == 0)
-		{
-			return (NULL);
-		}
+		if (s[i] == c)
+			return (s + i);
 	}
+
+	return ("\0");
 }
