b6741fa294ae6aba7af76ed9f25e0b1a4345976a
jabir254 <jabirkullow@gmail.com>
2022-11-03 21:44:59 +0300

    updated main.h

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,14 +4,15 @@
  *  @s : string paramenter
  * Return: Always 0.
  */
-void _puts_recursion(char *)
+void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s+1);
+	}
+	else
 	{
 		_putchar('\n');
-		return;
 	}
-	_putchar(*s);
-	s++;
-	_puts_recursion(s);
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,6 @@
-#ifndef "MAIN_H"
-#define "MAIN_H"
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -8,4 +9,5 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int is_prime_number(int n);
 int wildcmp(char *s1, char *s2);
+
 #endif
